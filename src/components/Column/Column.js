7b79059bd742aa282b4e41777260513560cbffea
Natalia Potracka <n.potracka@gmail.com>
2023-08-21 13:31:11 +0200

    add task 13.2

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,20 +1,26 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Column = (props) => {
-    return (
-      <article className={styles.column}>
-        <h2 className={styles.title}>
-          <span className={styles.icon + ' fa fa-' + props.icon}></span>
-          {props.title}
-        </h2>
-        {props.children}
-        <ul className={styles.cards}>{props.cards.map(card => <Card key={card.id} title={card.title}/>)}</ul>
-        <CardForm columnId={props.id} action={props.action} />
-      </article>
-    )
-  }
+  const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
 
-
+  return (
+    <article className={styles.column}>
+      <span className={styles.icon + ' fa fa-' + props.icon} />
+      <h2 className={styles.title}>{props.title}</h2>
+      <ul className={styles.cards}>
+        {cards.map((card) => (
+          <Card key={card.id} title={card.title} />
+        ))}
+      </ul>
+      <CardForm
+        columnId={props.id}
+        action={props.columnId}
+        addCard={props.addCard}
+      />
+    </article>
+  );
+};
 export default Column;
