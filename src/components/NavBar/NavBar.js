6b6f8f60c09e9e9d3846c60294957af98fd736f9
Natalia Potracka <n.potracka@gmail.com>
2023-08-24 16:47:40 +0200

    add task 14.3

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,20 +1,14 @@
 import styles from './NavBar.module.scss';
-import Container from '../Container/Container';
-import { Route } from 'react-router-dom';
-import Home from '../Home/Home';
-import Favorite from '../Favorite/Favorite';
-import About from '../About/About';
-import NotFound from '../NotFound/NotFound';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <nav className={styles.navbar}>
-        <span className="fa fa-bars"></span>
+        <Link to="/"><span className="fa fa-bars"></span></Link>
         <span className={styles.navtxt}>
-          <a href="/">Home</a>
-          <a href="/favorite">Favorite</a>
-          <a href="/about">About</a>
-          <a href="/not_found">Not Found</a>
+          <a><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/">Home</NavLink></a>
+          <a><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/favorite">Favorite</NavLink></a>
+          <a><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/about">About</NavLink></a>
         </span>
     </nav>
   );
--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -4,28 +4,51 @@ const initialState = {
       id: 1,
       title: 'Books',
       icon: 'book',
+      listId: '1',
     },
     {
       id: 2,
       title: 'Movies',
       icon: 'film',
+      listId: '1',
     },
     {
       id: 3,
       title: 'Games',
       icon: 'gamepad',
+      listId: '1',
+    },
+    {
+      id: 4,
+      title: 'Test',
+      icon: 'book',
+      listId: '2',
     }
+
   ],
 
   cards: [
-    { id: 1, columnId: 1, title: 'This is Going to Hurt' },
-    { id: 2, columnId: 1, title: 'Interpreter of Maladies' },
-    { id: 3, columnId: 2, title: 'Harry Potter' },
-    { id: 4, columnId: 2, title: 'Star Wars' },
-    { id: 5, columnId: 3, title: 'The Witcher' },
-    { id: 6, columnId: 3, title: 'Skyrim' }
+    { id: 1, columnId: 1, title: 'This is Going to Hurt', isFavourite: false },
+    { id: 2, columnId: 1, title: 'Interpreter of Maladies', isFavourite: false },
+    { id: 3, columnId: 2, title: 'Harry Potter', isFavourite: false },
+    { id: 4, columnId: 2, title: 'Star Wars', isFavourite: false },
+    { id: 5, columnId: 3, title: 'The Witcher', isFavourite: false },
+    { id: 6, columnId: 3, title: 'Skyrim', isFavourite: false },
   ],
-  
+
+  lists: [
+    {
+      id: '1',
+      title: 'Things to do...',
+      description: 'Interesting things I want to check out'
+    },
+    {
+      id: '2',
+      title: 'Test list',
+      description: 'Lorem Ipsum'
+    }
+  ],
+
   searchString: '',
 
 };
