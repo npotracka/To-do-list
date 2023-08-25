2ff83c86b245df6cf351a8e098695b1a78be100c
Natalia Potracka <n.potracka@gmail.com>
2023-08-25 15:28:16 +0200

    add task 14.5

--- src/redux/cardsReducer.js
+++ src/redux/cardsReducer.js
@@ -13,6 +13,8 @@ export const getFavoriteCard = (state) =>
 // action creators
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
 export const toggleCardFavorite = payload => ({ type: 'TOGGLE_CARD_FAVORITE', payload });
+export const removeCard = payload => ({ type: 'REMOVE_CARD', payload });
+
 
 export const cardsReducer = (statePart = [], action) => {
   switch(action.type) {
@@ -20,6 +22,8 @@ export const cardsReducer = (statePart = [], action) => {
       return [...statePart, { ...action.payload, id: shortid() }];
     case 'TOGGLE_CARD_FAVORITE':
       return statePart.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card);
+    case 'REMOVE_CARD':
+      return statePart.filter(card => card.id !== action.payload);
     default:
       return statePart;
   }
