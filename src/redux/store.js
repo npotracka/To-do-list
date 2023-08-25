9d614c1fd508e49d49abedda76e11594e9d9a96a
Natalia Potracka <n.potracka@gmail.com>
2023-08-25 11:51:47 +0200

    add task 14.4

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,12 +11,15 @@ export const getAllColumns = (state) => state.columns;
 export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId);
 export const getColumnsByList = ({ columns }, listId) => columns.filter(column => column.listId === listId);
 export const getAllLists = (state) => state.lists;
+export const getFavoriteCard = (state) =>
+  state.cards.filter((card) => card.isFavorite === true);
 
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
 export const updateSearchString = payload => ({ type: 'UPDATE_SEARCHSTRING', payload });
 export const addList = payload => ({ type: 'ADD_LIST', payload });
+export const toggleCardFavorite = payload => ({ type: 'TOGGLE_CARD_FAVORITE', payload });
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -30,10 +33,15 @@ const reducer = (state, action) => {
       };
     }
     case 'UPDATE_SEARCHSTRING': {
-      return { ...state, searchString: action.payload };
+      return { ...state, searchString: action.payload 
+      };
     }
     case 'ADD_LIST':
-      return { ...state, lists: [...state.lists, { ...action.payload, key: shortid(), id: shortid() }] };
+      return { ...state, lists: [...state.lists, { ...action.payload, key: shortid(), id: shortid() }],
+    }
+    case 'TOGGLE_CARD_FAVORITE':
+      return { ...state, cards: state.cards.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card) 
+    };
     default:
       return state;
   }
